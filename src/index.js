a919ba1352f2bb5986f7e49d5062f2f89b85b3fd
DaryaGorbachova <vrindavaneshvari@mail.ru>
2021-08-12 23:38:33 +0300

    done!! hooray!!!

--- src/index.js
+++ src/index.js
@@ -35,10 +35,38 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    ' ':      ' ',
 };
 
 function decode(expr) {
-    // write your solution here
+    let exprm = [];
+    let letter = '';
+// transform to normal morse code 
+
+   for (let j = 0; j < expr.length; j++) {
+       if (expr[j] === '1' && expr[j + 1] === '1')	{
+           letter += '-';
+           j++;
+       } else if (expr[j] === '1' && expr[j + 1] ==='0') {
+           letter += '.';
+           j++;
+       } else if (expr[j] === '*' && expr[j + 9] === '*') {
+           letter += ' ';
+           j += 9;
+       } 
+       if ((j + 1) % 10 === 0) {
+           exprm.push(letter);
+           letter = '';
+   }
+}
+// transform to text
+let result = '';
+for (let i = 0; i < exprm.length; i++){
+for (let k in MORSE_TABLE) {
+   if (exprm[i] === k)  result += MORSE_TABLE[k];
+}
+}
+   return result;
 }
 
 module.exports = {
